dc9542345cf07db7048e836452862f5682a66e66
tr-yasuda <5bpu76dxw@mozmail.com>
2024-06-05 20:41:11 +0900

    i18n 対応

---
+++ src/i18n.ts
@@ -0,0 +1,19 @@
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from 'react-i18next'
+import { en } from './locales/en.ts'
+import { ja } from './locales/ja.ts'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: process.env.NODE_ENV === 'development',
+    resources: {
+      en,
+      ja,
+    },
+  })
+
+export default i18n
---
+++ src/locales/en.ts
@@ -0,0 +1,10 @@
+export const en: { translation: Record<string, string> } = {
+  translation: {
+    'Create Amazing Videos': 'Create Amazing Videos',
+    Effortlessly: 'Effortlessly',
+    'Edit your videos with our intuitive and powerful tools. No experience required.':
+      'Edit your videos with our intuitive and powerful tools. No experience required.',
+    'Get Started': 'Get Started',
+    'Learn More': 'Learn More',
+  },
+}
