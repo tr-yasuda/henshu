5c90e1762d0d4d0ed8490c815a27b901749fb586
tr-yasuda <5bpu76dxw@mozmail.com>
2024-06-07 02:05:59 +0900

    コンポーネントの修正

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,20 +11,27 @@ type CardProps = {
     | 'warning'
     | 'error'
     | 'gray'
+    | 'white'
+  className?: string
 }
 
-export const Card = ({ children, color = 'primary' }: CardProps) => {
+export const Card = ({ children, color = 'primary', className }: CardProps) => {
   return (
     <div
-      className={clsx('rounded-lg shadow-lg overflow-hidden', {
-        'bg-primary-lighter': color === 'primary',
-        'bg-secondary-lighter': color === 'secondary',
-        'bg-info-lighter': color === 'info',
-        'bg-success-lighter': color === 'success',
-        'bg-warning-lighter': color === 'warning',
-        'bg-error-lighter': color === 'error',
-        'bg-gray-200': color === 'gray',
-      })}
+      className={clsx(
+        'p-6 rounded-lg shadow-lg overflow-hidden border-t-4',
+        {
+          'bg-primary-lighter': color === 'primary',
+          'bg-secondary-lighter': color === 'secondary',
+          'bg-info-lighter': color === 'info',
+          'bg-success-lighter': color === 'success',
+          'bg-warning-lighter': color === 'warning',
+          'bg-error-lighter': color === 'error',
+          'bg-gray-200': color === 'gray',
+          'bg-white': color === 'white',
+        },
+        className,
+      )}
     >
       {children}
     </div>
