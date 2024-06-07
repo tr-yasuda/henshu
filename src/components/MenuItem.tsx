fe8bef15a1ad02d79cc6276438d3a9eb617d3f76
tr-yasuda <5bpu76dxw@mozmail.com>
2024-06-08 02:09:37 +0900

    selected を使わないように修正

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -3,17 +3,8 @@ import type { ReactNode } from 'react'
 type MenuItemProps = {
   value?: string
   children: ReactNode
-  selected?: boolean
 }
 
-export const MenuItem = ({
-  value,
-  children,
-  selected = false,
-}: MenuItemProps) => {
-  return (
-    <option value={value} selected={selected}>
-      {children}
-    </option>
-  )
+export const MenuItem = ({ value, children }: MenuItemProps) => {
+  return <option value={value}>{children}</option>
 }
