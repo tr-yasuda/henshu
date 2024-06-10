7fb87dfa5f336688a79f9977cae178a2504635c7
tr-yasuda <5bpu76dxw@mozmail.com>
2024-06-10 15:15:04 +0900

    ログインフォーム作成

--- src/feature/login/components/LoginForm.tsx
+++ src/feature/login/components/LoginForm.tsx
@@ -1,23 +1,54 @@
 import { useForm } from '@tanstack/react-form'
 import { Link } from '@tanstack/react-router'
+import { zodValidator } from '@tanstack/zod-form-adapter'
 import { useTranslation } from 'react-i18next'
+import { z } from 'zod'
 import { Button } from '../../../components/Button.tsx'
+import { Card } from '../../../components/Card.tsx'
 import { Input } from '../../../components/Input.tsx'
 import BackgroundImage from '../images/background.webp'
 
 export const LoginForm = () => {
   const { t } = useTranslation()
-  // TODO: validate email and password
   const form = useForm({
     defaultValues: {
       email: '',
       password: '',
     },
+    validators: {
+      onMount: z.object({
+        email: z.string().email('Please enter your email address.'),
+        password: z
+          .string()
+          .min(12, 'Password must be at least 12 characters.'),
+      }),
+    },
+    validatorAdapter: zodValidator,
     onSubmit: async ({ value }) => {
       // TODO: handle login
       console.log(value)
     },
   })
+
+  const formFields: {
+    name: 'email' | 'password'
+    label: string
+    inputType: string
+    validator: z.ZodType
+  }[] = [
+    {
+      name: 'email',
+      label: 'Email',
+      inputType: 'email',
+      validator: z.string().email('Please enter your email address.'),
+    },
+    {
+      name: 'password',
+      label: 'Password',
+      inputType: 'password',
+      validator: z.string().min(12, 'Password must be at least 12 characters.'),
+    },
+  ]
 
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-100 relative'>
@@ -25,7 +56,7 @@ export const LoginForm = () => {
         className='absolute inset-0 bg-cover bg-center'
         style={{ backgroundImage: `url(${BackgroundImage})` }}
       />
-      <div className='max-w-md w-full bg-white p-8 rounded-lg shadow-md z-10'>
+      <Card fullWidth color={'white'} className='max-w-md z-10'>
         <div className='flex justify-center mb-6'>
           <img src={'./logo.webp'} alt='Henshu logo' className='h-12 w-auto' />
         </div>
@@ -33,63 +64,50 @@ export const LoginForm = () => {
         <p className='text-center text-gray-600 mb-6'>
           {t('Welcome back! Please login to your account.')}
         </p>
-        <form onSubmit={() => {}}>
-          <div className='mb-4'>
-            <form.Field name='email'>
-              {(field) => (
-                <>
-                  <label htmlFor='email' className='block text-gray-700'>
-                    {t('Email')}
-                  </label>
-                  <Input
-                    id='email'
-                    type='email'
-                    value={field.state.value}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                    className={'mt-1'}
-                    errorMessage={field.state.meta.errors.join(', ')}
-                    required
-                  />
-                </>
-              )}
-            </form.Field>
-          </div>
-          <div className='mb-4'>
-            <form.Field name='password'>
-              {(field) => (
-                <>
-                  <label htmlFor='password' className='block text-gray-700'>
-                    {t('Password')}
-                  </label>
-                  <Input
-                    id='password'
-                    type='password'
-                    value={field.state.value}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                    className={'mt-1'}
-                    errorMessage={field.state.meta.errors.join(', ')}
-                    required
-                  />
-                </>
-              )}
-            </form.Field>
-          </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            e.stopPropagation()
+            form.handleSubmit()
+          }}
+        >
+          {formFields.map((formField) => (
+            <div className='mb-4' key={formField.name}>
+              <form.Field
+                name={formField.name}
+                validators={{
+                  onChange: formField.validator,
+                }}
+              >
+                {(field) => (
+                  <>
+                    <label
+                      htmlFor={formField.name}
+                      className='block text-gray-700'
+                    >
+                      {t(formField.label)}
+                    </label>
+                    <Input
+                      id={formField.name}
+                      type={formField.inputType}
+                      value={field.state.value}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      errorMessage={field.state.meta.errors.toString()}
+                      required
+                    />
+                  </>
+                )}
+              </form.Field>
+            </div>
+          ))}
           <Button size={'xs'} variant={'text'} className='mb-4'>
             <div className='hover:underline'>{t('Forgot Password?')}</div>
           </Button>
           <form.Subscribe
-            selector={(state) => [
-              state.canSubmit,
-              state.isSubmitting,
-              state.isFieldsValid,
-            ]}
+            selector={(state) => [state.canSubmit, state.isDirty]}
           >
-            {([canSubmit, isSubmitting, isFieldsValid]) => (
-              <Button
-                type='submit'
-                fullWidth
-                disabled={!canSubmit && isSubmitting && !isFieldsValid}
-              >
+            {([canSubmit, isDirty]) => (
+              <Button type='submit' fullWidth disabled={!canSubmit || !isDirty}>
                 {t('Log In')}
               </Button>
             )}
@@ -105,7 +123,7 @@ export const LoginForm = () => {
             </Link>
           </p>
         </div>
-      </div>
+      </Card>
     </div>
   )
 }
